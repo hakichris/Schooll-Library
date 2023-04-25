7e3b5b31617b42db6b0d1e87602f290f1bacf3ca
Chris <hakizachrist1@gmail.com>
2023-04-26 00:34:24 +0200

    Update storage.rb

---
+++ run/storage.rb
@@ -0,0 +1,18 @@
+require 'json'
+
+class Storage
+  @file_extension = 'json'
+  @url = './db/'
+
+  def self.save_data(class_name, object)
+    p File.write("#{@url}#{class_name}.#{@file_extension}", JSON.generate(object))
+  end
+
+  def self.load_data(class_name)
+    file_path = "#{@url}#{class_name}.#{@file_extension}"
+    return [] unless File.exist?(file_path)
+
+    data_from = File.read(file_path)
+    JSON.parse(data_from, create_additions: true)
+  end
+end
