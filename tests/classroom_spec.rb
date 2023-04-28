f2813cb60a88eb1c2709b93a288f36638f26a347
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-04-28 12:44:36 +0000

    Add classrom spec file

--- tests/classroom_spec.rb
+++ tests/classroom_spec.rb
@@ -6,10 +6,8 @@ describe Classroom do
     it '>> Create a new object' do
       # Arrange
       @classroom = Classroom.new('Label')
-
       # Act
       label = @classroom.label
-
       # Assert
       expect(label).to eq 'Label'
     end
@@ -18,11 +16,9 @@ describe Classroom do
       # Arrange
       classroom = Classroom.new('Label')
       student = Student.new('25', 'ruby-101', 'Mohi', parent_permission: true)
-
       # Act
       classroom.add_student(student)
       number_of_students = classroom.students.length
-
       # Assert
       expect(number_of_students).to eq 1
     end
